1ef65bda8d9e25f060dfd509d05c2989d7ab0d0b
eotamm <eotamm@utu.fi>
2025-06-09 12:45:42 +0300

    Add files via upload

---
+++ main.R
@@ -0,0 +1,11 @@
+# Utility functions
+source("funcs.R")
+
+# Load and preprocess the data
+source("data.R")
+
+# Fit the survival model
+source("model.R")
+
+# Render the Quarto report
+quarto::quarto_render("survival.qmd")
